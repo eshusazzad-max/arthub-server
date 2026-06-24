1e9897ee465667664a1d29a5943a7e5a28918429
eshusazzad-max <eshusazzad@gmail.com>
2026-06-25 03:29:47 +0600

    Connect Express server with MongoDB Atlas

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("ArtHub Server Running");
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(5000, () => {
-  console.log("Server Running");
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("✅ MongoDB Connected");
+
+    app.get("/", (req, res) => {
+      res.send("ArtHub Server Running");
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
 });
