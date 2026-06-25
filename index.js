e6d23bcafa018e72f20bf0fc9212b36c2c96de68
eshusazzad-max <eshusazzad@gmail.com>
2026-06-25 12:03:03 +0600

    Create DELETE artwork API

--- index.js
+++ index.js
@@ -68,6 +68,16 @@ async function run() {
 
      res.send(result);
     });
+
+    app.delete("/artworks/:id", async (req, res) => {
+     const id = req.params.id;
+
+     const query = { _id: new ObjectId(id) };
+
+     const result = await artworksCollection.deleteOne(query);
+
+     res.send(result);
+    });
   } finally {
   }
 }
