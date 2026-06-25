91933d67bc34b098ef45cae54acdfe0aa588f8c3
eshusazzad-max <eshusazzad@gmail.com>
2026-06-25 12:42:55 +0600

    Create GET user by email API

--- index.js
+++ index.js
@@ -92,6 +92,16 @@ async function run() {
      const result = await usersCollection.find().toArray();
      res.send(result);
     });
+
+    app.get("/users/:email", async (req, res) => {
+     const email = req.params.email;
+
+     const query = { email: email };
+
+     const result = await usersCollection.findOne(query);
+
+     res.send(result);
+    });
   } finally {
   }
 }
