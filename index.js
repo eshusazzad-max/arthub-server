71dc54aea819dd596e17d980b497b6a549d51293
eshusazzad-max <eshusazzad@gmail.com>
2026-06-25 12:35:32 +0600

    Create GET users API

--- index.js
+++ index.js
@@ -87,6 +87,11 @@ async function run() {
 
      res.send(result);
     });
+
+    app.get("/users", async (req, res) => {
+     const result = await usersCollection.find().toArray();
+     res.send(result);
+    });
   } finally {
   }
 }
