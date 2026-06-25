726b81623817537a4784e9ea3c4094ce4c13c071
eshusazzad-max <eshusazzad@gmail.com>
2026-06-25 11:46:40 +0600

    Create GET single artwork API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,6 +41,16 @@ async function run() {
     app.post("/artworks", async (req, res) => {
      const newArtwork = req.body;
      const result = await artworksCollection.insertOne(newArtwork);
+     res.send(result);
+    });
+
+    app.get("/artworks/:id", async (req, res) => {
+     const id = req.params.id;
+
+     const query = { _id: new ObjectId(id) };
+
+     const result = await artworksCollection.findOne(query);
+
      res.send(result);
     });
   } finally {
